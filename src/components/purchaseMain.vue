4c2e735a3af6fdc1d89388c0343b0031aecaecac
Landon Harvey <landonharvey5@gmail.com>
2019-01-30 16:46:47 -0600

    commit

--- src/components/purchaseMain.vue
+++ src/components/purchaseMain.vue
@@ -5,11 +5,17 @@
         <h1>Product Search</h1>
       </div>
       <div id="searchContainer">
-        <input id="searchBar" type="text" placeholder="Search...">
+        <input id="searchBar" type="text" placeholder="Search..." v-model="ebaySearch">
         <div>
           <button id="searchButton" @click.once="fetchData(), created()">Search</button>
         </div>
-        {{this.info}}
+      </div>
+      <div id="ebaySearchContainer">
+        <div v-for="(item,key) in info" :key="key" id="ebaySearch">
+          <img class="ebayImg" :src="item.galleryURL" alt="NOPE">
+          <div>{{item.title[0]}}</div>
+          <div id="ebayPrice">PRICE: {{item.sellingStatus[0].currentPrice[0].__value__}}</div>
+        </div>
       </div>
       <div v-if="showResults" id="searchResults">
         <div v-for="(tvs, key) in walmartJson" :key="key" id="itemContainer">
@@ -20,70 +26,64 @@
 </template>
 
 <script>
-import walmartJson from '../assets/walmartsamsung'
-import axios from 'axios'
-
-export default {
-  name: 'purchaseMain',
-  data () {
-    return {
-      walmartJson,
-      showResults: false,
-      info: [],
-      errors: []
-    }
-  },
-  methods: {
-    fetchData () {
-      this.showResults = true
-      console.log()
+  import walmartJson from '../assets/walmartsamsung'
+  import axios from 'axios'
+  export default {
+    name: 'purchaseMain',
+    data () {
+      return {
+        walmartJson,
+        showResults: false,
+        info: [],
+        errors: [],
+        ebaySearch: ''
+      }
     },
-    created () {
-      let url = 'http://svcs.sandbox.ebay.com/services/search/FindingService/v1'
-      url += '?OPERATION-NAME=findItemsByKeywords'
-      url += '&SERVICE-VERSION=1.3.0'
-      url += '&SECURITY-APPNAME= APIKEY'
-      url += '&GLOBAL-ID=EBAY-US'
-      url += '&RESPONSE-DATA-FORMAT=JSON'
-      url += '&callback=_cb_findItemsByKeywords'
-      url += '&REST-PAYLOAD'
-      url += '&keywords=harry%20potter'
-      url += '&paginationInput.entriesPerPage=3'
-
-      axios.get(url)
-        .then(data => {
-          this.info = data
-        }).catch(error => {
+    methods: {
+      fetchData () {
+        this.showResults = true
+        console.log()
+      },
+      created () {
+        let url = 'https://cors-anywhere.herokuapp.com/http://svcs.sandbox.ebay.com/services/search/FindingService/v1'
+        url += '?OPERATION-NAME=findItemsByKeywords'
+        url += '&SERVICE-VERSION=1.0.0'
+        url += '&SECURITY-APPNAME='
+        url += '&GLOBAL-ID=EBAY-US'
+        url += '&RESPONSE-DATA-FORMAT=JSON'
+        url += '&keywords='
+        url += this.ebaySearch
+        url += '&REST-PAYLOAD'
+        url += '&paginationInput.entriesPerPage=3'
+        axios.get(url)
+          .then(data => {
+            this.info = data.data.findItemsByKeywordsResponse[0].searchResult[0].item
+            console.log(this.info[0].galleryURL[0])
+          }).catch(error => {
           console.log(error)
         })
+      }
     }
   }
-}
-
 </script>
 
 <style scoped>
-
   #searchContainer {
     display: flex;
     justify-content: center;
   }
-
   #searchBar {
     height: 3.5em;
     width: 59%;
   }
-
   #searchButton {
     height: 3.5em;
   }
-
   #searchResults {
     display: flex;
     flex-wrap: wrap;
     flex-direction: row;
   }
-
   #itemContainer {
     display: flex;
     flex-direction: column;
@@ -93,10 +93,26 @@ export default {
     width: 49%;
     padding-bottom: 1em;
   }
-
   img {
     width: 50%;
-
   }
+  #ebaySearchContainer{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
+  #ebaySearch {
+    display: flex;
+    width: 49%;
+    padding-bottom: 3em;
+  }
+  .ebayImg {
+    width: 100px;
+    height: 60px  ;
+  }
+  #ebayPrice {
+    float: right;
+  }
+
 
 </style>
